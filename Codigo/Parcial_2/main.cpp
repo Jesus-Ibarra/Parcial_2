56e7dedc243aca7a7ab66d4bec814d709ec8f1f7
Jesus-Ibarra <80048825+Jesus-Ibarra@users.noreply.github.com>
2021-09-24 22:13:48 -0500

    tratando con el sobremuestreo

--- Codigo/Parcial_2/main.cpp
+++ Codigo/Parcial_2/main.cpp
@@ -6,14 +6,89 @@ using namespace std;
 int main()
 {
 
-    int c=0;
+    int c=0,p,q,a,l,m,n,s,t;
     unsigned long long rojo,verde,azul;
-    string filname = "../Parcial_2/Mapas/Guayana.png"; //cargar la imagen
+    int AlturaInicial,AnchoInicial;
+    string filname = "../Parcial_2/Mapas/Colombia4x4.png"; //cargar la imagen
                     //retrocede/ingresa a la carpeta/nombre de la imagen o /nombre de la carp. imagen/nom. imagen
     QImage im(filname.c_str());
 
 
-    cout<<im.width()<<endl;
+    AlturaInicial=im.height();
+    AnchoInicial=im.width();
+     p=16/AlturaInicial;
+     q=16/AnchoInicial;
+     m=16%AlturaInicial;
+     n=16%AnchoInicial;
+     s=AlturaInicial*p+m;
+     t=AnchoInicial*q+n;
+    /*cout<<"ingrese a y b: ";
+    cin>>a>>l;
+    p=16/a;
+    q=16/l;
+    m=16%a;
+    n=16%l;
+    s=a*p+m;
+    t=l*q+n;
+    cout<<"division a: "<<p<<endl;
+    cout<<"modulo a: "<<m<<endl;
+    cout<<s<<endl;
+    cout<<"division l: "<<q<<endl;
+    cout<<"modulo l: "<<n<<endl;
+    cout<<t<<endl;*/
+
+
+
+
+
+   /* for(int indey=0;indey<AlturaInicial;indey+=1)
+    {
+        for(int index=0;index<AnchoInicial;index+=1)
+        {
+            for(int clock=0;clock<s;clock+=1)
+            {
+            rojo=im.pixelColor(index,indey).red();
+            verde=im.pixelColor(index,indey).green();
+            azul=im.pixelColor(index,indey).blue();
+            c++;
+            cout <<index<<", "<<indey<<" = "<< "rojo: "<<rojo<<'\t'<<"verde: "<<verde<<'\t'<<"azul: "<<azul<<endl;
+            }
+            //cout<<c<<endl;
+        }
+    }*/
+    string nombreArchivo= "../Parcial_2/Matriz.txt";
+    ofstream archivo;
+    c=0;
+    archivo.open(nombreArchivo.c_str(), fstream::out);
+    archivo<<"{"<<endl;
+
+    for(int indey=0;indey<AlturaInicial;indey+=1)
+    {
+        for(int clockY=0;clockY<s;clockY+=1)
+        {
+            archivo<<"{"<<endl;
+            for(int index=0;index<AnchoInicial;index+=1)
+            {
+                for(int clockX=0;clockX<t;clockX+=1)
+                {
+                    rojo=im.pixelColor(index,indey).red();
+                    verde=im.pixelColor(index,indey).green();
+                    azul=im.pixelColor(index,indey).blue();
+                    archivo <<"{"<<rojo<<","<<verde<<","<<azul<<"}";
+                    c=c+1;
+                    if(c%16!=0)
+                    {
+                        archivo<<",";
+                    }
+                }
+
+            }
+            archivo<<endl<<"},"<<endl;
+        }
+    }
+    archivo.close();
+
+ /*   cout<<im.width()<<endl;
     cout<<im.width()/16<<endl;
     cout<<im.height()<<endl;
     cout<<im.height()/16<<endl;
@@ -57,7 +132,7 @@ for(int indey=0;indey<im.height();indey+=im.height()/16)
 
     archivo<<endl<<"}"<<endl;
 }
-archivo.close();
+archivo.close();*/
 
     //ImagenRead n;
     //n.submuestreo();
