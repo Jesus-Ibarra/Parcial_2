842704e5ee309c81eb79fd7bee284435069e26dd
Jesus-Ibarra <80048825+Jesus-Ibarra@users.noreply.github.com>
2021-09-26 22:44:28 -0500

    avance del sobremuestreo 5x5 y 7x7

--- Codigo/Parcial_2/main.cpp
+++ Codigo/Parcial_2/main.cpp
@@ -6,11 +6,12 @@ using namespace std;
 int main()
 {
 
-    int c=0,p,q,a,l,m,n,s,t;
+    int c,p,n,a;//,q,a,l,m,n,s,t;
+    float A=0;
     unsigned long long rojo,verde,azul;
     int AlturaInicial,AnchoInicial;
     //char band=true,opcion=true;
-    string filname = "../Parcial_2/Mapas/Guayana4x5.png"; //cargar la imagen
+    string filname = "../Parcial_2/Mapas/Colombia9x9.png"; //cargar la imagen
                     //retrocede/ingresa a la carpeta/nombre de la imagen o /nombre de la carp. imagen/nom. imagen
     QImage im(filname.c_str());
 
@@ -41,41 +42,7 @@ int main()
 //**************************************************************************************************
   //escala=AlturaInicial*AnchoInicial;
 
-     /*ratio=3;
 
-     char **RGB[16][16][3];
-     x=round(AlturaInicial/16*i);
-     y=round(AnchoInicial/16*j);
-
-      rojo = 250;
-     [row,AnchoInicial,rojo]=size(filname);
-
-
-
-      row = round(ratio * row);
-      AnchoInicial = round(ratio * AnchoInicial);
-
-      //  Generar nueva imagen
-      dst = zeros(row, AnchoInicial, rojo, class(filname));
-
-      for (i = 1 : row){
-          for j = 1 : col{
-              x = round(i / ratio);
-              y = round(j / ratio);
-              if x == 0{
-                  x = x + 1;
-              }
-              if y == 0{
-                  y = y + 1;
-              }
-              dst(i, j, : ) = src(x, y, :);
-          }
-      }
-
-      figure
-      imshow(src)
-      figure
-      imshow(dst)*/
 //******************************************************************************************************************************
     /* char ***RGB;  //genera matriz
 
@@ -95,91 +62,7 @@ int main()
 
              cout<<"|"<<rojo<<","<<verde<<","<<azul<<"|"<<'\t';
 
-             *********************************************************
-
-             //m=i+1;
-             //n=j+1;
-             //p=i-m;
-             //q=j-n;
-
-             //s=(1-p)*(1-q)*(1,1)+p*(1-q)*(1,3)+(1-p)*q*(3,1)+p*q*(3,3);
-         }
-         cout<<endl;
-
-     }*/
-     /*s=(1-p)*(1-q)*(1,1)+p*(1-q)*(1,3)+(1-p)*q*(3,1)+p*q*(3,3);
-
-     string nombreArchivo= "../Parcial_2/Matriz.txt";
-     ofstream archivo;
-     c=0;
-     archivo.open(nombreArchivo.c_str(), fstream::out);
-     archivo<<"{"<<endl;
-
-     for(int indey=0;indey<AlturaInicial;indey+=1)
-     {
-         for(int clockY=0;clockY<s;clockY+=1)
-         {
-             archivo<<"{"<<endl;
-             for(int index=0;index<AnchoInicial;index+=1)
-             {
-                 for(int clockX=0;clockX<s;clockX+=1)
-                 {
-                     rojo=im.pixelColor(index,indey).red();
-                     verde=im.pixelColor(index,indey).green();
-                     azul=im.pixelColor(index,indey).blue();
-                     archivo <<"{"<<rojo<<","<<verde<<","<<azul<<"}";
-                     c=c+1;
-                     if(c%16!=0)
-                     {
-                         archivo<<",";
-                     }
-                 }
-
-             }
-             archivo<<endl<<"},"<<endl;
-         }
-     }
-     archivo.close();*/
-
-//**************************************************************************************************
-/*     string nombreArchivo= "../Parcial_2/Matriz.txt";
-     ofstream archivo;
-     c=0;
-     archivo.open(nombreArchivo.c_str(), fstream::out);
-     archivo<<"{"<<endl;
-
-     for(int indey=0;indey<AlturaInicial;indey+=1)
-     {
-         for(int index=0;index<AnchoInicial;index+=1)
-         {
-         for(int clockY=indey*escala;clockY<(indey*escala)+escala;clockY+=1)
-         {
-             archivo<<"{"<<endl;
-
-                 for(int clockX=index*escala;clockX<(index*escala)+escala;clockX+=1)
-                 {
-                     rojo=im.pixelColor(index,indey).red();
-                     verde=im.pixelColor(index,indey).green();
-                     azul=im.pixelColor(index,indey).blue();
-                     nuevo[clockX][clockY]=RGB[index][indey];
-                     nuevo[clockX][clockY]=new char (rojo);
-                     nuevo[clockX][clockY]=new char (verde);
-                     nuevo[clockX][clockY]=new char (azul);
-                     archivo <<"{"<<rojo<<","<<verde<<","<<azul<<"}";
-                     c=c+1;
-                     if(c%16!=0)
-                     {
-                         archivo<<",";
-                     }
-                 }
-
-             }
-             archivo<<endl<<"},"<<endl;
-         }
-     }
-     archivo.close();
-*/
-
+             **********************************************************/
 
 //************************************************************************************************
 
@@ -221,6 +104,9 @@ n=16%AnchoInicial;*/
             }
         }
     }*/
+
+ //****************************************PRINCIPAL***************************************************
+    /*
     string nombreArchivo= "../Parcial_2/Matriz.txt";
     ofstream archivo;
     c=0;
@@ -251,23 +137,17 @@ n=16%AnchoInicial;*/
                     c=c+1;
                     if(p!=0 && clockY>p){//q=16/AlturaInicial
 
-
                             clockY-=p;
                         //im.copy(index,indey,AnchoInicial,AlturaInicial);
                             //indey=p;
                             //index=p;
                             p--;
-
-
-
                     }
                      if(n!=0 && clockX>n ){//m=16/AnchoInicial
 
                             clockX-=n;
                             n--;
-
-
-                    }
+                    }*/
                     /*else if(p!=0 && clockY>p && n!=0 && clockX>n && indey>p && index>n){
 
                             clockY-=p;
@@ -278,7 +158,7 @@ n=16%AnchoInicial;*/
                             n--;
 
                     }*/
-                    if(c%16!=0)
+                   /* if(c%16!=0)
                     {
                         //index-=1;
                         archivo<<",";
@@ -289,7 +169,233 @@ n=16%AnchoInicial;*/
             archivo<<endl<<"},"<<endl;
         }
     }
+    //QImage guardar;
+    //string imagen="../Parcial_2/Mapas";
+    //guardar.save("../Parcial_2/Mapas/Colombia4x7copi.png","jpg");
+    archivo.close();*/
+    //******************************************************************************************
+    //************************COMPAÑERO**********************************************************
+
+    A=16%im.height();
+
+    string nombreArchivo= "../Parcial_2/Matriz.txt";
+    ofstream archivo;
+    c=0;
+    archivo.open(nombreArchivo.c_str(), fstream::out);
+    archivo<<"{"<<endl;
+
+    //pixeles faltantes
+
+
+
+
+    //4nx
+   /* for(int indey=0;indey<AlturaInicial;indey+=1)
+    {
+        for(int clockY=0;clockY<16/AlturaInicial;clockY+=1)
+        {
+            archivo<<"{"<<endl;
+            if(A!=0)
+            {
+                for(int index=0;index<AnchoInicial;index+=1)
+                {
+                    for(int clockX=0;clockX<16/AnchoInicial;clockX+=1)
+                    {
+                        rojo=im.pixelColor(index,indey).red();
+                        verde=im.pixelColor(index,indey).green();
+                        azul=im.pixelColor(index,indey).blue();
+                        archivo <<"{"<<rojo<<","<<verde<<","<<azul<<"}";
+                        c++;
+                        if(c%16!=0)
+                        {
+                            archivo<<",";
+                        }
+                    }
+                }
+                archivo<<endl<<"},"<<endl;
+                archivo <<"{"<< endl;
+                A--;
+            }
+            for(int index=0;index<AnchoInicial;index+=1)
+            {
+                for(int clockX=0;clockX<16/AnchoInicial;clockX+=1)
+                {
+                    rojo=im.pixelColor(index,indey).red();
+                    verde=im.pixelColor(index,indey).green();
+                    azul=im.pixelColor(index,indey).blue();
+                    archivo <<"{"<<rojo<<","<<verde<<","<<azul<<"}";
+                    c++;
+                    if(c%16!=0)
+                    {
+                        archivo<<",";
+                    }
+                }
+            }
+            archivo<<endl<<"},"<<endl;
+        }
+    }
     archivo.close();
+    */
+
+
+    //nxn
+
+   // string nombreArchivo= "../Parcial_2/Matriz.txt";
+   // ofstream archivo;
+   // c=0;
+   // archivo.open(nombreArchivo.c_str(), fstream::out);
+   // archivo<<"{"<<endl;
+
+    //pixeles faltantes
+
+
+
+
+    int PixelesFaltantes=16%im.width();
+    a=16/AnchoInicial;
+
+
+
+
+
+    for(int indey=0;indey<AlturaInicial;indey+=1)
+    {
+        for(int clockY=0;clockY<16/AlturaInicial;clockY+=1)
+        {
+            archivo<<"{"<<endl;
+
+            if(A!=0)
+            {
+                PixelesFaltantes=16%im.width();
+                for(int index=0;index<AnchoInicial;index+=1)
+                {
+                    for(int clockX=0;clockX<16/AnchoInicial;clockX+=1)
+                    {
+                        rojo=im.pixelColor(index,indey).red();
+                        verde=im.pixelColor(index,indey).green();
+                        azul=im.pixelColor(index,indey).blue();
+                        archivo <<"{"<<rojo<<","<<verde<<","<<azul<<"}";
+                        c++;
+                        if(PixelesFaltantes!=0)
+                            {
+                                for(int i=1;i<=PixelesFaltantes;i++){
+                                    clockX-=i;
+                                    PixelesFaltantes--;
+                                }
+                            }
+                        /*else if(PixelesFaltantes!=0 && PixelesFaltantes>a){
+                            //a=PixelesFaltantes-a;
+                            clockX=PixelesFaltantes-a;
+                            for(int i=1;i<=PixelesFaltantes;i++){
+                                clockX-=i;
+                                PixelesFaltantes--;
+                            }
+                        }*/
+                        if(c%16!=0)
+                        {
+                            archivo<<",";
+                        }
+                    }
+                }
+                archivo<<endl<<"},"<<endl;
+
+                archivo <<"{"<< endl;
+                A--;
+
+            }
+            PixelesFaltantes=16%im.width();
+            //for(PixelesFaltantes=16%im.width();PixelesFaltantes!=0;PixelesFaltantes--){
+                for(int index=0;index<AnchoInicial;index+=1)
+                {
+                    for(int clockX=0;clockX<16/AnchoInicial;clockX+=1)
+                    {
+
+                        rojo=im.pixelColor(index,indey).red();
+                        verde=im.pixelColor(index,indey).green();
+                        azul=im.pixelColor(index,indey).blue();
+                        archivo <<"{"<<rojo<<","<<verde<<","<<azul<<"}";
+                        c++;
+                        if(PixelesFaltantes!=0){
+                            for(int i=1;i<=PixelesFaltantes;i++){
+                            clockX-=i;
+                            PixelesFaltantes--;
+                            }
+                        }
+                        if(c%16!=0)
+                        {
+                            archivo<<",";
+                        }
+                        //clockX-=PixelesFaltantes;
+
+                    }
+
+                }
+
+            archivo<<endl<<"},"<<endl;
+        }
+    }
+
+
+   /* if(PixelesFaltantes!=0){
+        for(int indey=0;indey<AlturaInicial;indey+=1)
+        {
+            for(int clockY=0;clockY<16/AlturaInicial;clockY+=1)
+            {
+                archivo<<"{"<<endl;
+
+                if(A!=0)
+                {
+                    for(int index=0;index<AnchoInicial;index+=1)
+                    {
+                        for(int clockX=0;clockX<16/AnchoInicial;clockX+=1)
+                        {
+                            rojo=im.pixelColor(index,indey).red();
+                            verde=im.pixelColor(index,indey).green();
+                            azul=im.pixelColor(index,indey).blue();
+                            archivo <<"{"<<rojo<<","<<verde<<","<<azul<<"}";
+                            c++;
+                            if(c%16!=0)
+                            {
+                                archivo<<",";
+                            }
+                        }
+                    }
+                    archivo<<endl<<"},"<<endl;
+
+                    archivo <<"{"<< endl;
+                    A--;
+                }
+                for(int index=0;index<AnchoInicial;index+=1)
+                {
+                    for(int clockX=0;clockX<16/AnchoInicial;clockX+=1)
+                    {
+                        rojo=im.pixelColor(index,indey).red();
+                        verde=im.pixelColor(index,indey).green();
+                        azul=im.pixelColor(index,indey).blue();
+                        archivo <<"{"<<rojo<<","<<verde<<","<<azul<<"}";
+                        c++;
+                        if(c%16!=0)
+                        {
+                            archivo<<",";
+                        }
+                    }
+                }
+                archivo<<endl<<"},"<<endl;
+            }
+        }
+        archivo<<endl<<"},"<<endl;
+
+        archivo <<"{"<< endl;
+        PixelesFaltantes--;
+    }*/
+
+    archivo.close();
+
+
+
+
+
+    //************************************************************************************************
 
  /*   cout<<im.width()<<endl;
     cout<<im.width()/16<<endl;
@@ -362,65 +468,7 @@ archivo.close();*/
 
 
     //***************************************************************************************************
-/*int arreglo[16][16][3]=
-  {
-{
-{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0}
-},
-{
-{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0}
-},
-{
-{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0}
-},
-{
-{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0}
-},
-{
-{255,246,0},{255,246,0},{255,246,0},{255,246,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{252,243,0},{252,243,0},{252,243,0},{252,243,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0}
-},
-{
-{255,246,0},{255,246,0},{255,246,0},{255,246,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{252,243,0},{252,243,0},{252,243,0},{252,243,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0}
-},
-{
-{255,246,0},{255,246,0},{255,246,0},{255,246,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{252,243,0},{252,243,0},{252,243,0},{252,243,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0}
-},
-{
-{255,246,0},{255,246,0},{255,246,0},{255,246,0},{254,245,0},{254,245,0},{254,245,0},{254,245,0},{252,243,0},{252,243,0},{252,243,0},{252,243,0},{255,246,0},{255,246,0},{255,246,0},{255,246,0}
-},
-{
-{48,26,228},{48,26,228},{48,26,228},{48,26,228},{24,0,254},{24,0,254},{24,0,254},{24,0,254},{23,0,243},{23,0,243},{23,0,243},{23,0,243},{24,0,255},{24,0,255},{24,0,255},{24,0,255}
-},
-{
-{48,26,228},{48,26,228},{48,26,228},{48,26,228},{24,0,254},{24,0,254},{24,0,254},{24,0,254},{23,0,243},{23,0,243},{23,0,243},{23,0,243},{24,0,255},{24,0,255},{24,0,255},{24,0,255}
-},
-{
-{48,26,228},{48,26,228},{48,26,228},{48,26,228},{24,0,254},{24,0,254},{24,0,254},{24,0,254},{23,0,243},{23,0,243},{23,0,243},{23,0,243},{24,0,255},{24,0,255},{24,0,255},{24,0,255}
-},
-{
-{48,26,228},{48,26,228},{48,26,228},{48,26,228},{24,0,254},{24,0,254},{24,0,254},{24,0,254},{23,0,243},{23,0,243},{23,0,243},{23,0,243},{24,0,255},{24,0,255},{24,0,255},{24,0,255}
-},
-{
-{251,0,1},{251,0,1},{251,0,1},{251,0,1},{238,0,16},{238,0,16},{238,0,16},{238,0,16},{249,0,3},{249,0,3},{249,0,3},{249,0,3},{252,0,0},{252,0,0},{252,0,0},{252,0,0}
-},
-{
-{251,0,1},{251,0,1},{251,0,1},{251,0,1},{238,0,16},{238,0,16},{238,0,16},{238,0,16},{249,0,3},{249,0,3},{249,0,3},{249,0,3},{252,0,0},{252,0,0},{252,0,0},{252,0,0}
-},
-{
-{251,0,1},{251,0,1},{251,0,1},{251,0,1},{238,0,16},{238,0,16},{238,0,16},{238,0,16},{249,0,3},{249,0,3},{249,0,3},{249,0,3},{252,0,0},{252,0,0},{252,0,0},{252,0,0}
-},
-{
-{251,0,1},{251,0,1},{251,0,1},{251,0,1},{238,0,16},{238,0,16},{238,0,16},{238,0,16},{249,0,3},{249,0,3},{249,0,3},{249,0,3},{252,0,0},{252,0,0},{252,0,0},{252,0,0}
-}
-  };
 
-for(int i=0;i<16;i++){
-      for(int j=0;j<16;j++){
-       cout<<"rojo: "<<arreglo[i][j][0]<<'\t';
-       cout<<"verde: "<<arreglo[i][j][1]<<'\t';
-       cout<<"azul: "<<arreglo[i][j][2]<<endl;;
-      }
-    }*/
 
 
 
